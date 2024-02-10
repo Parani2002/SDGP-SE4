de3f291527563b781b445c1cf61d59e8d7fb1ef1
Paranitharan <paranitharanparani13@gmail.com>
2024-02-10 19:13:30 +0530

    Update app.py

--- Back_end/app.py
+++ Back_end/app.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask import render_template,request
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
+def home():
+    return render_template('Homepage.html')
 def index():
     return 'Hello World'
