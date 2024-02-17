8d0e5023a00d4e6da65d87c586acee8b0935f8eb
Paranitharan <paranitharanparani13@gmail.com>
2024-02-17 18:07:11 +0530

    flask update

--- Back_end/app.py
+++ Back_end/app.py
@@ -5,7 +5,35 @@ app = Flask(__name__)
 
 @app.route('/')
 @app.route('/home')
+
 def home():
-    return render_template('Homepage.html')
+    return render_template('index.html')
+
+#Route for career finder model
+@app.route('/career_finder')
+def career_finder():
+    return render_template('career_finder.html')
+
+#Route for the university finder
+@app.route('/university_finder')
+def university_finder():
+    return render_template('uinversity_finder.html')
+
+#Route for the login page
+@app.route('/login', methods =["GET"])
+def login():
+    return render_template('login.html')
+
+@app.route('/login', methods =['POST'])
+def loginForm():
+    email = request.form['email']
+    password = request.form['password']
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
 def index():
     return 'Hello World'
