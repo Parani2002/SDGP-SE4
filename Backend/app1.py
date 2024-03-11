dd8700934b1bc7c2968f6e91d6db7e481aa19371
Geerthanah Mathivathanan <138343450+geerthanah@users.noreply.github.com>
2024-03-11 20:12:31 +0530

    commented

--- Backend/app1.py
+++ Backend/app1.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from flask import render_template,request,url_for,jsonify
+#from flask import Flask
+#from flask import render_template,request,url_for,jsonify
 
 #app = Flask(__name__)
 #  route
