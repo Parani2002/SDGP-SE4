a02cc7e51a78115bc41b6b938789b2349cbf9727
Abdulmm03 <ammidhlaj003@gmail.com>
2024-03-13 16:05:12 +0530

    Delete Router.py

--- BACKEND/routes/Router.py
+++
@@ -1,9 +0,0 @@
-import express
-from controllers.propertyController import createProperty, deleteProperty, fullyUpdateProperty, getAllProperty, getProperty, parttiallyUpdateProperty
-
-propertyRouter = express.Router()
-propertyRouter.route("/").get(getAllProperty).post(createProperty)
-propertyRouter.route("/:id").get(getProperty).put(fullyUpdateProperty).patch(parttiallyUpdateProperty).delete(deleteProperty)
-
-export
-
