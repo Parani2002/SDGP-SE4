041259d4c805c35caa14f3e004396a1371f9d8d1
Paranitharan <paranitharanparani13@gmail.com>
2024-03-20 17:53:16 +0530

    update app.py

--- Back_end/app.py
+++ Back_end/app.py
@@ -2,6 +2,14 @@ from flask import Flask
 from flask import render_template,request,url_for,jsonify,redirect
 from pymongo import *
 from pymongo import MongoClient
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import LabelEncoder
+import pickle
+import pandas as pd
+import os
+import numpy as np
+
+
 
 #Establishing MongoDB connection
 cluster = MongoClient('mongodb://localhost:27017')
@@ -54,13 +62,82 @@ def signup_form():
 #Route for the quiz page
 @app.route('/quiz')
 def quiz():
-    return render_template('quiz.html')
+    return render_template('quiz2.html')
 
 
 #Route for the career page
 @app.route('/career')
 def career():
     return render_template('career.html')
+
+
+
+# Load the trained model
+with open('Back_end/naive_model.pkl', 'rb') as f:
+    mod = pickle.load(f)
+
+# Load the TF-IDF vectorizer
+tfidf = TfidfVectorizer()
+
+# Load the dataset
+path = 'Back_end/dataset.csv'
+df = pd.read_csv(path)
+df = df.rename(columns={'career name': 'type of career', 'in demand for the next 10 years?': 'in demand'})
+df['in demand'] = df['in demand'].map({'yes': 'in demand', 'no': 'not in demand'})
+tfidf.fit(df['type of career'])
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get input text from the HTML form
+    input_text = request.form['career-input']
+
+    # Transform input text using the TF-IDF vectorizer
+    text_features = tfidf.transform([input_text])
+
+    # Make prediction using the trained model
+    prediction = mod.predict(text_features)
+
+    # Return prediction to HTML page
+    return render_template('prediction.html', prediction=prediction[0])
+
+
+
+
+# Load the dataset
+path = 'Back_end/dataset1.csv'
+dataset = pd.read_csv(path)
+
+
+# Load the trained model
+with open('Back_end/knnModel.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+# Create an instance of LabelEncoder
+label_encoder = LabelEncoder()
+
+
+
+
+@app.route('/career_quiz', methods=['POST'])
+def career_quiz():
+    init_features=[int(x) for x in request.form.values()]
+    print(init_features)
+
+    transformed_data = label_encoder.fit_transform(dataset['Course'].unique())
+    print(transformed_data)
+
+    prediction = model.predict([init_features])
+    print(prediction)
+    
+    prediction = label_encoder.inverse_transform(prediction)
+    print(prediction)
+
+    
+    return render_template('prediction2.html',prediction_career = prediction[0])
+
+
+
+
 
 
 
@@ -89,29 +166,6 @@ def loginForm():
 
 
 #Signup into page and mongoDB connection
-# @app.route('/signup', methods=['GET', 'POST'])
-# def signup():
-#     print("connected")
-#     if request.method == 'POST':
-#         name = request.form['name']
-#         email = request.form['email']
-#         password = request.form['password']
-        
-#         #print("ssssss")
-#         user = {
-#             'name': name,
-#             'email': email,
-#             'password': password
-#         }
-#         print(user)
-#         collection.insert_one(user)
-        
-#         #after successful signup  you might want to redirect the user to another page
-#         return render_template("home.html")
-#     else:
-#         #handle GET request for signup page
-#         return redirect(url_for('signup_form'))  # Assuming you have a signup.html template
-
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     print("connected")
