ed74d7f3a0ade485d4d08932e4917681e7f62b2b
Rathusaigini <rathusaiginithava13@gmail.com>
2024-03-22 20:21:20 +0530

    changes

--- Backend/test.py
+++ Backend/test.py
@@ -1,21 +1,25 @@
 import unittest
-
+import json
 from app import app
 
-class FlaskTest(unittest.TestCase):
-    def test_home(self):
-        tester = app.test_client(self)
-        response = tester.get("/home")
-        statuscode = response.status_code
-        self.assertEqual(statuscode, 200)
+class TestFlaskAPI(unittest.TestCase):
 
-    def test_newsletter(self):
-        tester = app.test_client(self)
-        response = tester.get("news_letter")
-        statuscode = response.status_code
-        self.assertEqual(statuscode, 200)
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
 
+    def test_hello(self):
+        response = self.app.get('/hello')
+        data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['message'], 'Hello, World!')
 
+    def test_echo(self):
+        data = {'key': 'value'}
+        response = self.app.post('/echo', json=data)
+        received_data = json.loads(response.get_data(as_text=True))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(received_data, data)
 
 if __name__ == '__main__':
     unittest.main()
